09ce8fe6361c7180a1030b259e050070fd68b38e
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-11-08 11:16:28 +0530

    Iteration add

--- main.js
+++ main.js
@@ -27,3 +27,23 @@ console.log("------------------------")
 for(const value of myArray){
     console.log(value)
 }
+
+console.log(myObj[Symbol.iterator]);
+console.log(myArray[Symbol.iterator]);
+console.log(myObj[Symbol.iterator] ? 'iterable':'Non-iterable');
+console.log(myArray[Symbol.iterator] ? 'iterable':'Non-iterable');
+
+// const myObj2 = {
+//     id: 'C002',
+//     name: 'Ajan',
+//     address : 'Matale',
+//     [Symbol.iterator]:function*(){
+//         yield this.id;
+//         yield this.name;
+//         yield this.address;
+//     }
+// };
+// console.log(myObj2[Symbol.iterator] ? 'iterable':'Non-iterable');
+// for(const value of myObj2){
+//     consol.log(value);
+// }
